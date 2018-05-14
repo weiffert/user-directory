2fce31f3beb371c7dc983008a90c9e59bde2ab5a
weiffert <weiffert@gmail.com>
2018-05-14 13:42:21 -0400

    Changes text of second h1 element

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const button = document.querySelector('button');
 button.addEventListener('click', changeText);
 
 function changeText() {
-    const header = document.querySelector('h1');
-    header.textContent = 'Why\'d you push that button?';
+    const header = document.querySelectorAll('h1');
+    header[1].textContent = 'Why\'d you push that button?';
 }
