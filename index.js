e7a1de962af622e1abb6a9bd579cded7f9edce6d
weiffert <weiffert@gmail.com>
2018-05-15 10:01:27 -0400

    Get the input value via event target

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const form = document.querySelector('form#userForm');
 const handleSubmit = function(event) {
-    const heading = document.querySelector('h1');
-    heading.textContent = document.querySelector('input').value;
     event.preventDefault();
+    const heading = document.querySelector('h1');
+    heading.textContent = event.target.userName.value;
 }
 
 form.addEventListener('submit', handleSubmit);
