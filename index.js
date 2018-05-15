f7786f59b7d9ca1d009c123103e9c008b37fd0e2
weiffert <weiffert@gmail.com>
2018-05-15 11:15:09 -0400

    Append the user as a proper DOM element

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const form = document.querySelector('form#userForm');
 
+const handleSubmit = function (event) {
+    event.preventDefault();
+    const form = event.target;
+    const users = document.querySelector('#users');
+    const userName = form.userName.value;
+    const age = form.age.value;
+
+    const p = document.createElement('p');
+    p.textContent = `${userName}, ${age}`;
+    users.appendChild(p);
+
+    form.reset();
+    form.userName.focus();
+}
+
+form.addEventListener('submit', handleSubmit);
+
+/*
+Why'd this add a second <p></p>?
+const form = document.querySelector('form#userForm');
+
 const handleSubmit = function(event) {
     const form = event.target;
     event.preventDefault();
@@ -12,3 +33,4 @@ const handleSubmit = function(event) {
 }
 
 form.addEventListener('submit', handleSubmit);
+*/
