834d68cc9e1299f87acea7695923fe26670b4cc8
weiffert <weiffert@gmail.com>
2018-05-15 10:57:06 -0400

    Add age and reset the form

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const form = document.querySelector('form#userForm');
 
 const handleSubmit = function(event) {
+
     event.preventDefault();
     const users = document.querySelector('#users');
-    users.innerHTML += `<p>${event.target.userName.value}<\p>`;
-    event.target.userName.value = '';
+    users.innerHTML += `<p>
+        ${event.target.userName.value}, ${event.target.age.value}
+        <\p>`;
+    form.reset();
 }
 
 form.addEventListener('submit', handleSubmit);
