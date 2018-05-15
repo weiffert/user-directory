388673451ef4455a5c40d43d7c0363c076365302
weiffert <weiffert@gmail.com>
2018-05-15 13:55:19 -0400

    Remove commented code from previous steps

--- index.js
+++ index.js
@@ -41,21 +41,3 @@ const handleSubmit = function (event) {
 }
 
 form.addEventListener('submit', handleSubmit);
-
-/*
-Why'd this add a second <p></p>?
-const form = document.querySelector('form#userForm');
-
-const handleSubmit = function(event) {
-    const form = event.target;
-    event.preventDefault();
-    const users = document.querySelector('#users');
-    const userName = form.userName.value;
-    const age = form.age.value;
-    users.innerHTML += `<p>${userName}, ${age}<\p>`;
-    form.reset();
-    form.userName.focus();
-}
-
-form.addEventListener('submit', handleSubmit);
-*/
