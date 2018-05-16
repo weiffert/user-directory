122a13d28c2f7d76841d645a22ebf60580711dbd
weiffert <weiffert@gmail.com>
2018-05-16 15:24:53 -0400

    Filter by name

--- index.js
+++ index.js
@@ -3,23 +3,23 @@ const userList = [];
 const userForm = document.querySelector('form#userForm');
 const filterForm = document.querySelector('form#filterForm');
 
-const renderColor = function(color) {
+const renderColor = function (color) {
     const box = document.createElement('div');
     box.style.width = '2rem';
     box.style.height = '1rem';
     box.style.background = color;
-    box.style.display ='inline-block';
+    box.style.display = 'inline-block';
     return box;
 }
 
-const renderProfile = function(data) {
+const renderProfile = function (data) {
     const box = document.createElement('div');
     box.classList.add('profile');
     box.style.backgroundImage = `url(${data})`;
     return box;
 }
 
-const renderListItem = function(label, value) {
+const renderListItem = function (label, value) {
     const item = document.createElement('li');
     const dt = document.createElement('dt');
     const dd = document.createElement('dd');
@@ -27,7 +27,7 @@ const renderListItem = function(label, value) {
     dt.textContent = label.length > 0 ? `${label}: ` : '';
     try {
         dd.appendChild(value);
-    } catch(error) {
+    } catch (error) {
         dd.textContent += value;
     }
     item.appendChild(dt);
@@ -35,14 +35,14 @@ const renderListItem = function(label, value) {
     return item;
 }
 
-const renderList = function(data) {
+const renderList = function (data) {
     const list = document.createElement('dl');
     list.style.border = `.25rem solid ${data['Favorite Color'].style.background}`;
-    
+
     const keys = Object.keys(data);
     keys.forEach(key => {
         list.appendChild(renderListItem(
-            key === keys[0] ? '' : key, 
+            key === keys[0] ? '' : key,
             data[key]
         ));
     });
@@ -68,25 +68,32 @@ const handleSubmit = function (event) {
     form.userName.focus();
 }
 
-const handleFilter = function(event) {
+const appendList = function (userList) {
+    console.log(userList);
+    const users = document.querySelector('#users');
+    userList.forEach(function (user) {
+        users.appendChild(renderList(user));
+    });
+}
+
+const handleFilter = function (event) {
     event.preventDefault();
     const form = event.target;
     const value = form.filter.value;
     const users = document.querySelector('div#users');
-    
-    while(users.children.length > 0) {
+
+    while (users.children.length > 0) {
         users.removeChild(users.children[0]);
     }
     users.appendChild(form);
-    
-    appendList(userList);
-}
 
-const appendList = function(userList) {
-    const users = document.querySelector('#users');
-    userList.forEach(function(user) {
-        users.appendChild(renderList(user));
-    });
+    if (value === '')
+        appendList(userList);
+    else
+        appendList(
+            userList.filter(function (user) {
+                return user['Name'].indexOf(value) >= 0;
+            }));
 }
 
 userForm.addEventListener('submit', handleSubmit);
